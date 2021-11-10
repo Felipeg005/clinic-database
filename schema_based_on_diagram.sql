28b733fd9e807808661b0c164dd042f2ab085877
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-10 10:17:28 -0600

    Add foreign keys to all tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,15 +2,19 @@ CREATE TABLE patients (
   id INT GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
   PRIMARY KEY (id),
   name VARCHAR,
-  date_of_birth DATE
+  date_of_birth DATE,
+  CONSTRAINT kf_patients FOREIGN KEY(id) REFERENCES medical_histories(patient_id) ON DELETE CASCADE
 );
+
+CONSTRAINT kf_species FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE, 
 
 CREATE TABLE medical_histories (
   id INT GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
   PRIMARY KEY (id),
   admitted_at TIMESTAMP,
   patient_id INT,
-  status VARCHAR
+  status VARCHAR,
+  CONSTRAINT kf_treatments FOREIGN KEY(id) REFERENCES treatments(treatment_id) ON DELETE CASCADE
 );
 
 CREATE TABLE invoices (
@@ -19,7 +23,9 @@ CREATE TABLE invoices (
   total_amount DECIMAL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
-  medical_history_id INT
+  medical_history_id INT,
+  CONSTRAINT kf_medical_histories FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
+  CONSTRAINT kf_invoice_items FOREIGN KEY(id) REFERENCES invoice_items(invoice_id) ON DELETE CASCADE
 );
 
 CREATE TABLE invoice_items (
@@ -29,7 +35,7 @@ CREATE TABLE invoice_items (
   quantity INT,
   total_price DECIMAL,
   invoice_id INT,
-  treatment_id INT
+  treatment_id INT,
 );
 
 CREATE TABLE treatments (
@@ -37,5 +43,6 @@ CREATE TABLE treatments (
   PRIMARY KEY (id),
   type VARCHAR,
   name VARCHAR
+  CONSTRAINT kf_invoice_items FOREIGN KEY(id) REFERENCES invoice_items(treatment_id) ON DELETE CASCADE
 );
 
